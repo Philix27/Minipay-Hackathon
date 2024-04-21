c6e9ed53420d301f596f1c3966ec5ebb3957e1f6
Philix27 <philixbob@gmail.com>
2024-04-21 21:38:18 +0100

    fix and stable

--- client/app/lib/providers/providers.tsx
+++ client/app/lib/providers/providers.tsx
@@ -3,7 +3,6 @@
 import React, { ReactNode } from "react"
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 import { ThemeProvider } from "next-themes"
-import { WagmiProvider } from "wagmi"
 
 import { TrpcProvider } from "./trpcProvider"
 
@@ -12,11 +11,9 @@ const queryClient = new QueryClient()
 export function AppProviders(props: { children: ReactNode }) {
   return (
     <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-      {/* <WagmiProvider config={wagmiConfig}> */}
       <QueryClientProvider client={queryClient}>
         <TrpcProvider>{props.children}</TrpcProvider>
       </QueryClientProvider>
-      {/* </WagmiProvider> */}
     </ThemeProvider>
   )
 }
