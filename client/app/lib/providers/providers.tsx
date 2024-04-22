5e722775b66b90bec5083b2932c1a6420c3864c1
Philix27 <philixbob@gmail.com>
2024-04-22 09:16:45 +0100

    client: added web3 modal

--- client/app/lib/providers/providers.tsx
+++ client/app/lib/providers/providers.tsx
@@ -1,36 +1,30 @@
-"use client"
+"use client";
 
-import React, {
-  Dispatch,
-  ReactNode,
-  SetStateAction,
-  createContext,
-  useState,
-} from "react"
-// Import CELO chain information
-import { Alfajores, Celo } from "@celo/rainbowkit-celo/chains"
-// Import known recommended wallets
-import { Valora } from "@celo/rainbowkit-celo/wallets"
-import {
-  RainbowKitProvider,
-  connectorsForWallets,
-  getDefaultConfig,
-} from "@rainbow-me/rainbowkit"
-import { ledgerWallet } from "@rainbow-me/rainbowkit/wallets"
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
-import { ThemeProvider } from "next-themes"
-import { Toaster } from "sonner"
-import { WagmiConfig, WagmiProvider } from "wagmi"
-import { arbitrum, base, celo, celoAlfajores, mainnet } from "wagmi/chains"
+import React, { Dispatch, ReactNode, SetStateAction, createContext, useState } from "react";
+import { RainbowKitProvider, connectorsForWallets, getDefaultConfig } from "@rainbow-me/rainbowkit";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ThemeProvider } from "next-themes";
+import { Toaster } from "sonner";
+import { WagmiProvider } from "wagmi";
+import { arbitrum, base, celo, celoAlfajores, mainnet } from "wagmi/chains";
 
-import { TrpcProvider } from "./trpcProvider"
 
-const config = getDefaultConfig({
-  appName: "My RainbowKit App",
-  projectId: "90987766665",
+
+import { TrpcProvider } from "./trpcProvider";
+
+
+const rainbowConfig = getDefaultConfig({
+  appName: "Xpensa",
+  projectId: "a1ca0e06558ccfc78d859bff77e9d666",
   chains: [mainnet],
   ssr: true, // If your dApp uses server side rendering (SSR)
 })
+// const wagmiConfig = getDefaultConfig({
+//   appName: "Xpensa",
+//   projectId: "90987766665",
+//   chains: [mainnet],
+//   ssr: true, // If your dApp uses server side rendering (SSR)
+// })
 
 const queryClient = new QueryClient()
 export const SettingsContext = createContext<{
@@ -45,7 +39,7 @@ export function AppProviders(props: { children: ReactNode }) {
   const [isOpen, setOpen] = useState<boolean>(false)
   return (
     <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-      <WagmiProvider config={config}>
+      <WagmiProvider config={rainbowConfig}>
         <QueryClientProvider client={queryClient}>
           <RainbowKitProvider>
             <TrpcProvider>
