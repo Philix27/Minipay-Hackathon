bf13dee1864755523081355732216157c2048620
Philix27 <philixbob@gmail.com>
2024-04-22 09:00:11 +0100

    fix wagmi err

--- client/app/lib/providers/providers.tsx
+++ client/app/lib/providers/providers.tsx
@@ -7,28 +7,28 @@ import React, {
   createContext,
   useState,
 } from "react"
+// Import CELO chain information
+import { Alfajores, Celo } from "@celo/rainbowkit-celo/chains"
+// Import known recommended wallets
+import { Valora } from "@celo/rainbowkit-celo/wallets"
+import {
+  RainbowKitProvider,
+  connectorsForWallets,
+  getDefaultConfig,
+} from "@rainbow-me/rainbowkit"
+import { ledgerWallet } from "@rainbow-me/rainbowkit/wallets"
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 import { ThemeProvider } from "next-themes"
 import { Toaster } from "sonner"
+import { WagmiConfig, WagmiProvider } from "wagmi"
+import { arbitrum, base, celo, celoAlfajores, mainnet } from "wagmi/chains"
 
 import { TrpcProvider } from "./trpcProvider"
-import { RainbowKitProvider, getDefaultConfig } from "@rainbow-me/rainbowkit"
-import { ledgerWallet } from "@rainbow-me/rainbowkit/wallets"
-import { WagmiProvider } from "wagmi"
-import {
-  arbitrum,
-  base,
-  celo,
-  celoAlfajores,
-  mainnet,
-  optimism,
-  polygon,
-} from "wagmi/chains"
 
 const config = getDefaultConfig({
   appName: "My RainbowKit App",
   projectId: "90987766665",
-  chains: [mainnet ],
+  chains: [mainnet],
   ssr: true, // If your dApp uses server side rendering (SSR)
 })
 
